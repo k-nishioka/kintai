1b67f5d011123471a580d2b69a9b9500c77522d5
LeonardoMatusaki <l-matusaki@cmind-co.jp>
2019-09-20 13:24:23 +0900

    create header design

---
+++ includes/template-parts/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>勤怠管理システム</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header id="header">
+    <div class="inner">
+        <nav class="content-between">
+            <h2 class="header-title">勤怠管理システム</h2>
+            <ul class="header-list">
+                <li class="header-item"><a href="" class="btn header-btn">ログイン</a></li>
+                <li class="header-item"><a href="" class="btn header-btn">ユーザー登録</a></li>
+                <li class="header-item"><a href="" class="btn header-btn btn-warning">ログアウト</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>勤怠管理システム</title>
-    <link rel="stylesheet" href="./reset.css">
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
+
+
+<?php require_once(dirname(__FILE__) . "/includes/template-parts/header.php"); ?>
 
 
 
-</body>
-</html>
+
+<?php require_once(dirname(__FILE__) . "/includes/template-parts/footer.php"); ?>
